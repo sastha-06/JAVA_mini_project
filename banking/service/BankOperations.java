5e53c4357c0eb650c569a30ee600d8f16015a75d
sastha-06 <sasthajeyasri.24ad@kct.ac.in>
2026-04-01 20:42:16 +0530

    Delete banking directory

--- banking/service/BankOperations.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import model.Account;
-import exception.BankException;
-
-public interface BankOperations {
-    void deposit(Account acc, double amt);
-    void withdraw(Account acc, double amt) throws BankException;
-}
